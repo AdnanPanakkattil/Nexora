c30d8553b3e97ecce3d400e517ee675dad546ec5
AdnanPanakkattil <muhammedadnan1247@gmail.com>
2026-07-30 19:01:17 +0530

    categories

--- Modules/ProductManagement/resources/views/Categories-list.blade.php
+++ Modules/ProductManagement/resources/views/Categories-list.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.backendlayouts')
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('page-css/patient.css') }}">
+<link rel="stylesheet" href="{{ asset('page-css/Text.css') }}">
 <style>
     #loader-overlay {
         display: none;
@@ -21,6 +21,165 @@
         transform: translate(-50%, -50%);
         z-index: 9999;
     }
+
+    /* uplode image start */
+.upload-wrapper{
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+
+    border:2px dashed #c9b5ff;
+    border-radius:18px;
+
+    background:linear-gradient(135deg,#faf8ff,#ffffff);
+
+    padding:40px;
+    min-height:250px;
+}
+
+.upload-left{
+    width:60%;
+}
+
+.upload-content{
+    cursor:pointer;
+    text-align:center;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+}
+
+.upload-icon{
+    font-size:65px;
+    color:#7c4dff;
+    margin-bottom:15px;
+}
+
+.upload-content h5{
+    font-weight:700;
+    color:#222;
+}
+
+.upload-content p{
+    color:#777;
+    margin-bottom:5px;
+}
+
+.upload-content small{
+    color:#999;
+}
+
+.browse-btn{
+    display:inline-flex;
+    align-items:center;
+    gap:8px;
+
+    padding:12px 25px;
+
+    background:#6f42ff;
+    color:#fff;
+
+    border-radius:10px;
+    font-weight:600;
+
+    transition:.3s;
+}
+
+.browse-btn:hover{
+    background:#5a32d4;
+}
+
+/* Right */
+
+.preview-right{
+    width:35%;
+    display:flex;
+    justify-content:center;
+}
+
+.preview-circle{
+
+    position:relative;
+
+    width:180px;
+    height:180px;
+
+    border-radius:50%;
+    background:#f4efff;
+
+    border:8px solid white;
+
+    box-shadow:0 10px 30px rgba(0,0,0,.12);
+
+    overflow:hidden;
+}
+
+.preview-image{
+
+    width:100%;
+    height:100%;
+
+    object-fit:cover;
+    border-radius:50%;
+}
+
+.placeholder-avatar{
+
+    width:100%;
+    height:100%;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    font-size:60px;
+    color:#b7a8ff;
+}
+
+.remove-image{
+
+    position:absolute;
+
+    top:8px;
+    right:8px;
+
+    width:42px;
+    height:42px;
+
+    border:none;
+    border-radius:50%;
+
+    background:#ff3b5c;
+    color:white;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    box-shadow:0 8px 20px rgba(255,59,92,.35);
+}
+
+.remove-image:hover{
+    transform:scale(1.08);
+}
+
+/* Responsive */
+
+@media(max-width:768px){
+
+.upload-wrapper{
+    flex-direction:column;
+    gap:30px;
+}
+
+.upload-left,
+.preview-right{
+    width:100%;
+}
+
+}
+
+    /* uplode image end  */
 </style>
 @endpush
 
@@ -43,9 +202,9 @@
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h4 class="fw-bold py-3 mb-0"> Categories</h4>
 
-        <a href="{{ route('Categories') }}" class="btn btn-primary">
-            <i class="ti ti-plus me-1"></i> Add Category
-        </a>
+        <button type="button" class="btn btn-primary" id="addCategoryModalBtn">
+            <i class="menu-icon tf-icons ti ti-plus"></i>Add Category
+        </button>
     </div>
     <div class="card">
         <div class="card-datatable table-responsive p-3">
@@ -76,6 +235,103 @@
         </div>
     </div>
 </div>
+
+<!-- catogery modal start-->
+
+<div class="modal fade" id="addCategoryModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Category</h5>
+
+                <button type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close">
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <form id="addCategoryForm">
+                    <input type="hidden" id="category_id">
+                    <div class="mb-3 row">
+                        <div class="col-md-12 mb-4">
+
+                            <label class="form-label fw-semibold">
+                                Category Image
+                            </label>
+
+                           <div class="upload-wrapper">
+
+    <!-- Left Upload Content -->
+    <div class="upload-left">
+        <input type="file" id="memberPhoto" accept="image/*" hidden>
+
+        <label for="memberPhoto" class="upload-content" id="uploadContent">
+            <i class="ti ti-cloud-upload upload-icon"></i>
+
+            <h5>Drag & drop an image here</h5>
+            <p>or click to browse</p>
+
+            <small>PNG, JPG, WEBP • Max 2MB</small>
+
+            <div class="browse-btn mt-3">
+                <i class="ti ti-folder"></i>
+                Browse Files
+            </div>
+        </label>
+    </div>
+
+    <!-- Right Preview -->
+    <div class="preview-right">
+
+        <div class="preview-circle">
+
+            <img id="imagePreview"
+                class="preview-image d-none">
+
+            <div class="placeholder-avatar" id="avatarPlaceholder">
+                <i class="ti ti-photo"></i>
+            </div>
+
+            <button type="button"
+                id="removeImage"
+                class="remove-image d-none">
+                <i class="ti ti-trash"></i>
+            </button>
+
+        </div>
+
+    </div>
+
+</div>
+
+                        </div>
+                        <div class="col-md-6">
+                            <label for="categoryName_en" class="form-label labe">Category Name (EN)</label>
+                            <input placeholder="Name English" type="text" class="form-control" id="categoryName_en" name="categoryName_en">
+                            <span class="text-danger error-text categoryName_en_error"></span>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="categoryName_ar" class="form-label labe">Category Name (AR)</label>
+                            <input placeholder="Name Arabic" type="text" class="form-control" id="categoryName_ar" name="categoryName_ar">
+                            <span class="text-danger error-text categoryName_ar_error"></span>
+                        </div>
+                    </div>
+
+                    <div class="mt-5" style="display: flex;justify-content: center;">
+                        <button type="button" class="btn btn-primary" id="CategorySaveBtn">Save</button>
+                        <button type="button" class="btn btn-secondary" id="closebtn">Close</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- catogery modal end-->
+
 
 @endsection
 
--- public/page-js/productManagement/Categories.js
+++ public/page-js/productManagement/Categories.js
@@ -11,5 +11,95 @@ $(function () {
     });
     // Sidebar menu active state - End
 
+
+    //Add catogery modal upending
+
+    $("#addCategoryModalBtn").on("click", function () {
+        $("#addCategoryModal").modal("show");
+    })
+
+    //Add catogery modal upending end
+
+    // Initialize DataTable for categories Start
+
+    var table = $("#categoriesTable").DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: "/product-management/categories",
+            type: "GET",
+        },
+        columns: [
+            { data: "id", name: "id" },
+            { data: "category_image", name: "category_image" },
+            { data: "category_name_en", name: "category_name_en" },
+            { data: "category_name_ar", name: "category_name_ar" },
+            { data: "status", name: "status", orderable: false, searchable: false },
+            {
+                data: 'OrderNo',
+                name: 'OrderNo',
+                orderable: true,
+                searchable: false,
+                render: function (data, type, row, meta) {
+                    var test = meta.row + meta.settings._iDisplayStart + 1;
+                    if (row.canEdit) {
+                        return `
+                    <button type="button" class="btn btn-info increment-category" data-id="${row.categoryId}">
+                        <i class="fa-solid fa-arrow-up"></i>
+                    </button>
+                    ${meta.row + meta.settings._iDisplayStart + 1}
+                    <button type="button" class="btn btn-info decrement-category" data-id="${row.categoryId}">
+                        <i class="fa-solid fa-arrow-down"></i>
+                    </button>
+                `;
+                    } else {
+                        return meta.row + meta.settings._iDisplayStart + 1;
+                    }
+                }
+            },
+            { data: "action", name: "action", orderable: false, searchable: false },
+        ],
+    })
+
+    // Initialize DataTable for categories end
+
+   const input = document.getElementById('memberPhoto');
+const preview = document.getElementById('imagePreview');
+const removeBtn = document.getElementById('removeImage');
+const placeholder = document.getElementById('avatarPlaceholder');
+
+input.addEventListener('change', function () {
+
+    if (this.files && this.files[0]) {
+
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            preview.src = e.target.result;
+
+            preview.classList.remove('d-none');
+            removeBtn.classList.remove('d-none');
+
+            placeholder.classList.add('d-none');
+        }
+
+        reader.readAsDataURL(this.files[0]);
+    }
+
+});
+
+removeBtn.addEventListener('click', function () {
+
+    input.value = "";
+
+    preview.src = "";
+    preview.classList.add('d-none');
+
+    removeBtn.classList.add('d-none');
+
+    placeholder.classList.remove('d-none');
+
+});
 });
 
